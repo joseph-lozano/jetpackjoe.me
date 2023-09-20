e89da68fb9ce78e85d3c9ce933d06047c0d09b36
Joseph Lozano <me@josephlozano.xyz>
2023-09-20 10:23:14 -0400

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Awesome Blog";
+export const SITE_SUB_TITLE = "That's what I'm doing in my freetime.";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_URL = "https://sunny-arithmetic-1e866e.netlify.app";
